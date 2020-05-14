4747760d784833b11c92441925eb5c44600fa0f0
Amatus Muscheites <amatus.muscheites@gmail.com>
2020-05-14 11:58:40 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Teamgreeter from "./components/exampleComponent/Teamgreeter"
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Teamgreeter>Hello, Team!</Teamgreeter>
+      </header>
+    </div>
+  );
+}
+
+export default App;
