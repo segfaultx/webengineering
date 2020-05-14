a6c2933a10c10661c9117b5f44ec06e06168fc8c
Amatus Muscheites <amatus.muscheites@gmail.com>
2020-05-14 12:25:07 +0200

    added bootstrap dependency, added bootstrap example button

--- src/components/exampleComponent/Teamgreeter.js
+++ src/components/exampleComponent/Teamgreeter.js
@@ -1,9 +1,12 @@
 import React from "react"
+import Button from "react-bootstrap/Button"
+import "bootstrap/dist/css/bootstrap.min.css"
 
 const Teamgreeter = ({ children }) => {
-    return <h1>
+    return <div><h1>
         { children }
     </h1>
+    <Button variant="primary">Bootstrap Button, yay</Button></div>
 }
 
 export default Teamgreeter
