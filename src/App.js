5e48cf9106f8f496c9be38614b56058b812a8559
Amatus Muscheites <amatus.muscheites@gmail.com>
2020-05-17 15:05:09 +0200

    added register route

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import FormComp from "./components/formcomponent/Formcomponent"
 import loginToServer from "./components/util/LoginHandler"
-import {BrowserRouter as Router, Switch, Route, Link, Redirect} from "react-router-dom";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Redirect
+} from "react-router-dom"
 
 function App() {
     return (
@@ -11,13 +16,24 @@ function App() {
             <Router>
                 <Switch>
                     <Route path={"/login"}>
-                        <FormComp btnText={"Login"} func={loginToServer} errormsg={"Invalid credentials"} link={"/register"}/>
+                        <FormComp btnText={"Login"}
+                                  func={loginToServer}
+                                  errormsg={"Invalid credentials"}
+                                  link={"/register"}
+                                  linkText={"New? Register!"}/>
+                    </Route>
+                    <Route path={"/register"}>
+                        <FormComp btnText={"Register"}
+                                  func={null}
+                                  errormsg={"Username already taken"}
+                                  link={"/login"}
+                                  linkText={"Already have an account? Log in!"}/>
                     </Route>
                 </Switch>
                 <Redirect to={{pathname: "/login"}}/>
             </Router>
         </div>
-    );
+    )
 }
 
-export default App;
+export default App
