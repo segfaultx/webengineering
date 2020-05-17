1ee5be573570179acf86bc9e4328ed088b0df5f9
Amatus Muscheites <amatus.muscheites@gmail.com>
2020-05-17 14:46:19 +0200

    added router to app.js, redirecting to login at first

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import FormComp from "./components/formcomponent/Formcomponent"
-import login from "./components/util/LoginHandler"
+import loginToServer from "./components/util/LoginHandler"
+import {BrowserRouter as Router, Switch, Route, Link, Redirect} from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-        <FormComp btnText={"Login"} func={login} errormsg={"Invalid credentials"} link={"/register"}/>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+            </header>
+            <Router>
+                <Switch>
+                    <Route path={"/login"}>
+                        <FormComp btnText={"Login"} func={loginToServer} errormsg={"Invalid credentials"} link={"/register"}/>
+                    </Route>
+                </Switch>
+                <Redirect to={{pathname: "/login"}}/>
+            </Router>
+        </div>
+    );
 }
 
 export default App;
