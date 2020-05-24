9be82270dd1c29bde5d3ed3e4238874eb4f7c023
Amatus Muscheites <amatus.muscheites@gmail.com>
2020-05-24 12:16:12 +0200

    moved up redirectOnSuccess, clearData and useHistory

--- src/components/formcomponent/Formcomponent.js
+++ src/components/formcomponent/Formcomponent.js
@@ -1,5 +1,6 @@
 import React, {useState} from "react"
 import {Link} from "react-router-dom"
+import { useHistory } from "react-router-dom"
 import Form from "react-bootstrap/Form"
 import Button from "react-bootstrap/Button"
 import Container from "react-bootstrap/Container"
@@ -8,31 +9,32 @@ import Alert from "react-bootstrap/Alert"
 import "bootstrap/dist/css/bootstrap.min.css"
 import "./formComponentStyle.css"
 
-const FormComp = ({btnText, func, errormsg, link, linkText}) => {
+const FormComp = ({btnText, sendDataToServer, errormsg, link, linkText, formState, setState, redirectOnSuccess}) => {
+    const history = useHistory()
 
-    const [formState, setState] = useState({
-        "username": "",
-        "password": ""
-    })
     const [showAlert, setShowAlert] = useState(false)
 
     function handleChange(target, value) {
         setState({...formState, [target]: value})
     }
 
-    function handleLogin(username, pass) {
-        func(username, pass).then((response) => {
+    function handleSubmit(username, pass) {
+        sendDataToServer(username, pass).then((response) => {
+            setState({"username": "", "password": ""})
             if(!response){
                 setShowAlert(true)
                 return
             }
-            console.log("made it past")
+            history.push(redirectOnSuccess)
         })
     }
     function checkKeyboardEvent(key){
         if (key === "Enter"){
-            handleLogin(formState.username, formState.password)
+            handleSubmit(formState.username, formState.password)
         }
+    }
+    function clearProps(){
+        setState({"username": "", "password": ""})
     }
 
     return <Container fluid className={"bgContainer"}>
@@ -76,13 +78,13 @@ const FormComp = ({btnText, func, errormsg, link, linkText}) => {
                         </Form.Group>
                         <Container className={"formBtnContainer"}>
                             <Button variant="primary"
-                                    onClick={() => handleLogin(formState.username, formState.password)}
+                                    onClick={() => handleSubmit(formState.username, formState.password)}
                                     className={"formBtn"} aria-controls={"fade-alert"}
                                     aria-expanded={showAlert}>{btnText}</Button>
                         </Container>
                     </Form>
                 </Row>
-                <Link to={link} className={"formFont registerLink"}>{linkText}</Link>
+                <Link to={link} className={"formFont registerLink"} onClick={clearProps}>{linkText}</Link>
             </Container>
         </Row>
     </Container>
