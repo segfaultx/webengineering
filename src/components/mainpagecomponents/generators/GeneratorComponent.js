8f552db6b31da3313e0e72cf300c32f843123c06
Dennis Benert <dennis_benert96@web.de>
2020-05-24 12:19:02 +0200

    ordered GeneratorList

--- src/components/mainpagecomponents/generators/GeneratorComponent.js
+++ src/components/mainpagecomponents/generators/GeneratorComponent.js
@@ -4,10 +4,21 @@ import Container from "react-bootstrap/Container";
 
 const GeneratorComponent =({id,income_rate,order})=>{
 
+    const buyGenerator=()=>{
+        const requestOptions={
+            method:'GET',
+            headers:{
+                'Authorization':"Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJieWtvZiIsImV4cCI6MTU5MDM5NjMzOX0.RZjZkMGwn0WA_AZVNOD7WRxA_gkW5HGYZFNsIjIuwCo"           } //bearer token von postman mit bykof 123456
+        }
+        fetch("http://server.bykovski.de:8000/generators/"+id+"/buy",requestOptions)
+            .then(response=>response.json())
+            .then(data=>alert(data.detail))
+    }
+
     return(
         <Container>
             <h4>{`id:${id} IncomeRate:${income_rate} order:${order}`}</h4>
-            <Button>Buy</Button>
+            <Button onClick={buyGenerator}>Buy</Button>
         </Container>
     )
 }
