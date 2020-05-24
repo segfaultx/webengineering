405001d575589a6020505afd9ecc32bac42cfc8f
Amatus Muscheites <amatus.muscheites@gmail.com>
2020-05-24 12:16:45 +0200

    updated loginRequest, set cookie

--- src/components/util/LoginHandler.js
+++ src/components/util/LoginHandler.js
@@ -1,19 +1,35 @@
-import Config from "../../config";
-async function loginToServer(username, pass) {
+import Config from "../../config"
+import Cookie from "js-cookie"
+
+async function postLoginToServer(username, pass) {
+    let requestBody = new URLSearchParams()
+    requestBody.append("username", username)
+    requestBody.append("password", pass)
     return await fetch(`${Config.serverUrl}/users/token`, {
         method: "POST",
-        body: JSON.stringify({"username": username, "password": pass})
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        credentials: 'same-origin',
+        body: requestBody
     })
         .then(result => {
             if (result.status === 200) {
                 return result.json()
             }
-                return false
+            return null
         })
         .catch(error => {
             console.error(error)
-            return false
+            return null
         })
 }
 
+async function loginToServer(username, pass){
+    let token = null
+    await postLoginToServer(username, pass).then(value => token = value)
+    if (token !== null){
+        Cookie.set("authorization", token.access_token, {httpOnly: true, authorization: "strict", secure:true})
+    }
+}
 export default loginToServer
