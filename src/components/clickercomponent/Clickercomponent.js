1462b5ad87c5534d8f170560d5d569c8e8347c6f
Amatus Muscheites <amatus.muscheites@gmail.com>
2020-05-25 09:13:58 +0200

    initialized websocket via useEffect, installed handlers
    might need to move it to context

--- src/components/clickercomponent/Clickercomponent.js
+++ src/components/clickercomponent/Clickercomponent.js
@@ -1,17 +1,30 @@
 import React from "react"
 import {useEffect, useState} from "react"
 import Cookies from "js-cookie"
+import Config from "../../config";
 
-const Clickercomponent = ({websocket}) => {
+const Clickercomponent = ({initialCounterValue = 0}) => {
+    const [ws, setWs] = useState(null)
+    const [counter, setCounter] = useState(initialCounterValue)
     useEffect(() => {
-        websocket.onmessage = (message) => {
-            console.log(message)
-        }
-    })
-    const [counter, setCounter] = useState(0)
-    function handleClick () {
-        websocket.send(`token=${Cookies.get("token")}`)
+        let initWs = new WebSocket(`${Config.websocketUrl}/game/click?token=${Cookies.get("token")}`)
+        initWs.onmessage = handleUpdate
+        setWs(initWs)
+    }, [])
+
+    function handleUpdate(message){
+        console.log(message)
+        setCounter(counter + JSON.parse(message.data)["points"])
     }
+
+    function handleClick() {
+        if (ws !== null) {
+            console.log("click send")
+            ws.send(`token=${Cookies.get("token")}`)
+        }
+
+    }
+
     return <div>
 
         <button onClick={handleClick}>Click me! Counter: {counter}</button>
