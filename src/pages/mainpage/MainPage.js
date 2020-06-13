2c4b2fd808d2b0abbd5c24ac0512e249103f385a
saskiaschwarz <saskia.schwartz@student.hs-rm.de>
2020-06-13 16:37:55 +0200

    monster,avatar load planes load

--- src/pages/mainpage/MainPage.js
+++ src/pages/mainpage/MainPage.js
@@ -6,6 +6,13 @@ import dImg1 from "../../components/media/images/login_background_screen.jpg"
 import dImg2 from "../../components/media/images/UHpNxb.jpg"
 import dImg3 from "../../components/media/images/kJVa6H.jpg"
 import dImg4 from "../../components/media/images/kZPyQB.jpg"
+import PlayerPlane from "../../components/media/images/Player-Plane.png"
+import level1Monster from"../../components/media/images/monster/Megapack III Undead Warrior Benkei.png"
+import level2Monster from"../../components/media/images/monster/Megapack III Red Guard A.png"
+import level3Monster from"../../components/media/images/monster/Megapack III Fallen Kings Arcane King Jeffroy.png"
+import level5Monster from"../../components/media/images/monster/Megapack III Zodiac Aries.png"
+import level4Monster from"../../components/media/images/monster/Megapack III Elemental Lords Ice Maiden.png"
+import MonsterPlane from"../../components/media/images/Monster-Plane.png"
 import dImg5 from "../../components/media/images/binary-numbers-tunnel.jpg"
 import MainPageHeader from "../../components/mainpagecomponents/headercomponent/MainPageHeaderComponent"
 import GeneratorListComponent from "../../components/mainpagecomponents/generators/GeneratorListComponent"
@@ -13,10 +20,8 @@ import UpgradeListComponent from "../../components/mainpagecomponents/upgrades/U
 import {Container, Row, Col, Button} from "react-bootstrap"
 import {ClickContextProvider} from "../../contexts/clickContext"
 import {CPSContextProvider} from "../../contexts/cpsContext"
-import {GenerateArmyContextProvider} from "../../contexts/generateArmyContext"
 import Cookies from "js-cookie"
 import Clickercomponent from "../../components/mainpagecomponents/clickercomponent/Clickercomponent"
-import ArmyArea from "../../components/mainpagecomponents/generators/ArmyArea";
 
 
 const MainPage = () => {
@@ -27,6 +32,11 @@ const MainPage = () => {
         "backgroundCounter": 0
     })
 
+    const [monsterImage,setMonsterImage]=useState({
+        "monsterImages":[level1Monster,level2Monster,level3Monster,level4Monster,level5Monster],
+        "currentMonster": level1Monster,
+        "upgradeCounter": 0
+    })
 
 
     function handleBackgroundChange() {
@@ -55,43 +65,48 @@ const MainPage = () => {
     let render = <Redirect to={"/login"}/>
     if (Cookies.get("token")) {
         render = <Container className="wrapper" fluid style={background}>
-            <GenerateArmyContextProvider>
-                <ClickContextProvider>
-                    <CPSContextProvider>
-                        <Row className="maincontainer">
-                            <h2 className="gamename" style={{color: "white"}}>ClickerGame</h2>
+            <ClickContextProvider>
+            <CPSContextProvider>
+                <Row className="maincontainer">
+                    <h2 className="gamename" style={{color: "white"}}>ClickerGame</h2>
                             <MainPageHeader/>
-                        </Row>
+                </Row>
 
-                        <Row className="playarea">
-                            <Col>
-                                <GeneratorListComponent className="playareaComponents"/>
-                            </Col>
-                            <Col xs={6} >
-                                <Row className={"bossArea"}>
-                                    <Col>
-                                        <h2>BossArea</h2>
-                                        <Button name="backgroundCounter" variant="dark" className={"changeBackground"} onClick={() => {
-                                            handleBackgroundChange()}}>
-                                            change Background
-                                        </Button>
-                                        <Clickercomponent initialCounterValue={0}/>
-                                    </Col>
+                <Row className="playarea">
+                    <Col>
+                            <GeneratorListComponent className="playareaComponents"/>
+                    </Col>
 
-                                </Row>
-                                <Row className={"armyArea"}>
-                                    <Col>
-                                        <ArmyArea/>
-                                    </Col>
-                                </Row>
-                            </Col>
-                            <Col>
-                                <UpgradeListComponent className="playareaComponents"/>
-                            </Col>
-                        </Row>
-                    </CPSContextProvider>
-                </ClickContextProvider>
-            </GenerateArmyContextProvider>
+
+                    <Col xs={7} className={"mainPage"}>
+                        <h2>Mainpage</h2>
+                        <Button name="backgroundCounter" variant="dark" className={"changeBackground"} onClick={() => {
+                            handleBackgroundChange()}}>
+                            change Background
+                        </Button>
+
+
+                    </Col>
+
+
+
+
+                    <Col>
+                        <UpgradeListComponent className="playareaComponents"/>
+                    </Col>
+                    <Col>
+                    <img className={"monsterPlane"} src={monsterImage.currentMonster}/>
+                    <img src={MonsterPlane}/>
+                    </Col>
+                    <img className={"playerPlane"} src={PlayerPlane}/>
+                </Row>
+            </CPSContextProvider>
+            </ClickContextProvider>
+            <Row>
+                <Col>
+                    <Clickercomponent initialCounterValue={0}/>
+                </Col>
+            </Row>
         </Container>
     }
     return render
