cf74c60895cabcb8ae414e27546babda0c74cf1c
Christian Zekovic <chris.512@hotmail.de>
2020-06-26 10:12:45 +0200

    added background images

--- src/contexts/levelUpContext.js
+++ src/contexts/levelUpContext.js
@@ -1,9 +1,13 @@
 import React, {createContext, useState} from "react"
 import dImg1 from "../components/media/images/login_background_screen.jpg";
-import dImg2 from "../components/media/images/UHpNxb.jpg";
+import dImg2 from "../components/media/images/dungeon1.jpg";
 import dImg3 from "../components/media/images/kJVa6H.jpg";
-import dImg4 from "../components/media/images/kZPyQB.jpg";
-import dImg5 from "../components/media/images/binary-numbers-tunnel.jpg";
+import dImg4 from "../components/media/images/dungeon5.jpg";
+import dImg5 from "../components/media/images/kZPyQB.jpg";
+import dImg6 from "../components/media/images/dungeon2.jpg";
+import dImg7 from "../components/media/images/dungeon3.jpg";
+import dImg8 from "../components/media/images/dungeon4.jpg";
+import dImg9 from "../components/media/images/binary-numbers-tunnel.jpg";
 import level1Monster from "../components/media/images/monster/Megapack III Undead Warrior Benkei.png";
 import level2Monster from "../components/media/images/monster/Megapack III Red Guard A.png";
 import level3Monster from "../components/media/images/monster/Megapack III Fallen Kings Arcane King Jeffroy.png";
@@ -16,8 +20,8 @@ import level8Monster from "../components/media/images/monster/Megapack III Drago
 export const LevelUpContext= createContext()
 export const LevelUpContextProvider=({children})=>{
     const [backgroundImage, setBackgroundImage] = useState({
-        "backgroundImages": [dImg1, dImg2, dImg3, dImg4, dImg5],
-        "currentBackground": dImg1,
+        "backgroundImages": [dImg1, dImg2, dImg3, dImg4, dImg5, dImg6, dImg7, dImg8, dImg9],
+        "currentBackground": dImg2,
         "backgroundCounter": 0
     });
 
