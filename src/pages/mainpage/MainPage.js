1099dc7389ea041cff48cf359e336dbf3a9d8723
Amatus Muscheites <amatus.muscheites@gmail.com>
2020-07-01 13:07:47 +0200

    moft playerplane down, updated css accordingly

--- src/pages/mainpage/MainPage.js
+++ src/pages/mainpage/MainPage.js
@@ -1,19 +1,8 @@
 import React, {useContext, useState} from "react"
-import {useHistory} from "react-router-dom"
 import {Redirect} from "react-router-dom"
 import "./mainpagecomponentstyle.css"
-import dImg1 from "../../components/media/images/backgrounds/login_background_screen.jpg"
-import dImg2 from "../../components/media/images/backgrounds/cthulu_dungeon.jpg"
-import dImg3 from "../../components/media/images/backgrounds/text_wall.jpg"
-import dImg4 from "../../components/media/images/backgrounds/dungeon_stairs.jpg"
 import PlayerPlane from "../../components/media/images/generators/Player-Plane.png"
-import level1Monster from "../../components/media/images/monster/Megapack III Undead Warrior Benkei.png"
-import level2Monster from "../../components/media/images/monster/Megapack III Red Guard A.png"
-import level3Monster from "../../components/media/images/monster/Megapack III Fallen Kings Arcane King Jeffroy.png"
-import level5Monster from "../../components/media/images/monster/Megapack III Zodiac Aries.png"
-import level4Monster from "../../components/media/images/monster/Megapack III Elemental Lords Ice Maiden.png"
 import MonsterPlane from "../../components/media/images/clickercomp/Monster-Plane.png"
-import dImg5 from "../../components/media/images/backgrounds/binary-numbers-tunnel.jpg"
 import MainPageHeader from "../../components/mainpagecomponents/headercomponent/MainPageHeaderComponent"
 import GeneratorListComponent from "../../components/mainpagecomponents/generators/GeneratorListComponent"
 import UpgradeListComponent from "../../components/mainpagecomponents/upgrades/UpgradeListComponent"
@@ -28,7 +17,6 @@ import {VolumeContextProvider} from "../../contexts/volumeContext";
 import {ArmyAmountProvider} from "../../contexts/armyAmountContext";
 import {LevelUpContext, LevelUpContextProvider} from "../../contexts/levelUpContext";
 import {BoughtUpgradeContext, BoughtUpgradeContextProvider} from "../../contexts/boughtUpgradesContext";
-import BackgroundComponent from "../../components/mainpagecomponents/backgroundComponent/BackgroundComponent";
 import avatarConfig from "./avatarConfig";
 
 const MainPage = () => {
@@ -89,7 +77,7 @@ const MainPage = () => {
                                             <UpgradeListComponent className="playareaComponents"/>
                                         </Col>
                                     </Row>
-                                    <Row>
+                                    <Row className={"playerPlaneContainer"}>
                                         <img className={"playerPlane"} src={PlayerPlane}/>
                                     </Row>
                                 </Container>
