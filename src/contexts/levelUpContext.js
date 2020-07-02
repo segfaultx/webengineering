ce913f2a46f5d70a57017288d512b8031a1b468d
Christian Zekovic <chris.512@hotmail.de>
2020-07-02 09:36:41 +0200

    added final monster

--- src/contexts/levelUpContext.js
+++ src/contexts/levelUpContext.js
@@ -16,6 +16,7 @@ import level5Monster from "../components/media/images/monster/Megapack III Zodia
 import level6Monster from "../components/media/images/monster/Megapack III Elemental Lords Fire Lord.png";
 import level7Monster from "../components/media/images/monster/Megapack III Black Iron Minotaur Beef.png";
 import level8Monster from "../components/media/images/monster/Megapack III Dragonslayer Dwarf.png"
+import level9Monster from "../components/media/images/monster/Megapack III Hellhound Inferni.png"
 
 export const LevelUpContext= createContext()
 export const LevelUpContextProvider=({children})=>{
@@ -26,7 +27,8 @@ export const LevelUpContextProvider=({children})=>{
     });
 
     const [monsterImage, setMonsterImage] = useState({
-        "monsterImages": [level1Monster, level2Monster, level3Monster, level4Monster, level5Monster,level6Monster,level7Monster,level8Monster],
+        "monsterImages": [level1Monster, level2Monster, level3Monster, level4Monster, level5Monster,level6Monster,
+            level7Monster,level8Monster, level9Monster],
         "currentMonster": level1Monster,
         "upgradeCounter": 0
     });
