dc553ee6a02a10e91aa85b5b2249df8fb5539f8a
Amatus Muscheites <amatus.muscheites@gmail.com>
2020-07-04 14:09:03 +0200

    updated tests to actually test the async code

--- src/pages/register/RegisterHandler.test.js
+++ src/pages/register/RegisterHandler.test.js
@@ -1,7 +1,10 @@
 import registerUser from "./RegisterHandler"
 
-test("registerUser from RegisterHandler", () => {
-    expect(
+test("registerUser from RegisterHandler", done => {
+    function callback(response){
+        expect(response).toBeTruthy()
+        done()
+    }
         registerUser(Math.random().toString(), Math.random().toString())
-    ).toBeTruthy()
+            .then(response => callback(response))
 })
