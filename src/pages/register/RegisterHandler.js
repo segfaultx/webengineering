d441ec3eaa23df498362179dcd8e4e8d3ca38ef5
Amatus Muscheites <amatus.muscheites@gmail.com>
2020-07-07 08:23:26 +0200

    removed semicolon, comments

--- src/pages/register/RegisterHandler.js
+++ src/pages/register/RegisterHandler.js
@@ -1,5 +1,12 @@
 import Config from "../../config"
 
+/**
+ * Method to send a post request to server in an attempt to register the user,
+ * returns true if success, else false
+ * @param username
+ * @param pass
+ * @returns {Promise<boolean>}
+ */
 async function registerUser(username, pass) {
     let response = await fetch(`${Config.serverUrl}/users/register`, {
         method: "POST",
